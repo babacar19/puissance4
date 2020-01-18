91a87a43efb33f599a761aa83dd074a4ad83a13c
babacar19 <56703388+babacar19@users.noreply.github.com>
2020-01-18 23:37:15 +0100

    approche bizarre

--- index_js.js
+++ index_js.js
@@ -2,12 +2,18 @@ window.onload = function()
 {
     var conteneur = document.getElementById('beginning');
     var canvas = document.createElement('canvas');
-    
+    var ctx = canvas.getContext('2d');
+    var  heigthNumber = 6, widthNumber = 7;
+
+    ctx.beginPath();
+
     init();
 
     function init(){
-        var canvasWidth = 600;
+        var canvasWidth = 700;
         var canvasHeigth = 600;
+        var blocksize = 100;
+        
         canvas.width = canvasWidth;
         canvas.height = canvasHeigth;
         
@@ -15,17 +21,36 @@ window.onload = function()
         canvas.style.border = "1px solid black";
         conteneur.appendChild(canvas);
         
-        var ctx = canvas.getContext('2d');
+        
         canvas.style.border ='50px solid gray';
         canvas.style.textAlign = 'center';
         canvas.style.backgroundColor = 'blue';
 
-        drawCircles();
+        
+        initCircles(blocksize);
     }
 
-
-    function drawCircles(blocksize,blockNumber){
+    
+    function drawCircle(blocksize,abciss,ordinate){
         
+        ctx.save();
+        var radius = 0.25 * blocksize;
+        ctx.fillStyle = 'white';
+        ctx.arc(abciss * blocksize + radius , ordinate * blocksize + radius, radius,0, 2 * Math.PI);
+        ctx.fill();
+        ctx.restore();
+        
+        
+    }
+
+    function initCircles(blocksize){
+        
+        for( var i = 0; i < widthNumber; i++)
+        {
+            ctx.fillStyle = 'blue';
+            for(var j = 0; j < heigthNumber; j ++ )
+                drawCircle(blocksize,i,j);
+        }
 
     }
 
